692be32c2ac8fcaaf9e9ed8f723ec3ca337fcaee
Mehmet DERTLİ <dertli06mehmet@gmail.com>
2022-05-28 20:30:46 +0300

    first commit

---
+++ src/CartSummary.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+  Badge,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import {Link} from "react-router-dom";
+
+export default class CartSummary extends Component {
+  renderSummary() {
+    return (
+      <UncontrolledDropdown inNavbar nav>
+        <DropdownToggle caret nav>
+          Your Cart
+        </DropdownToggle>
+        <DropdownMenu>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              <Badge
+                color="danger"
+                onClick={() => this.props.removeFromCart(cartItem.product)}
+              >
+                X
+              </Badge>
+              {cartItem.product.productName}
+              <Badge color="success">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+
+          <DropdownItem divider />
+          <DropdownItem>
+            <Link to="cart">Go to cart</Link>
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+  renderEmptyCart() {
+    return (
+      <NavItem>
+        <NavLink>Empty Cart</NavLink>
+      </NavItem>
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.props.cart.length > 0
+          ? this.renderSummary()
+          : this.renderEmptyCart()}
+      </div>
+    );
+  }
+}
---
+++ src/Navi.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import CartSummary from "./CartSummary";
+import { Link } from "react-router-dom";
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar color="light" expand="md" light>
+          <NavbarBrand href="/">Nortwind App</NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() {}} />
+          <Collapse navbar>
+            <Nav className="ms-auto" navbar>
+              <NavItem>
+                <NavLink>
+                  <Link to="form1">Form Demo 1</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="form2">Form Demo 2</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <CartSummary
+                removeFromCart={this.props.removeFromCart}
+                cart={this.props.cart}
+              />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
